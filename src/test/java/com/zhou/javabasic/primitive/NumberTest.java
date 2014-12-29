7de3e94cd6432ecc680b07ff6fa72dae78d2a4cc
Zhou Li <zhoulijoe@gmail.com>
2014-12-29 01:49:53 -0800

    Create project for testing basic java syntax

---
+++ src/test/java/com/zhou/javabasic/primitive/NumberTest.java
@@ -0,0 +1,14 @@
+package com.zhou.javabasic.primitive;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class NumberTest {
+
+   @Test
+   public void createIntegerVariable() {
+      int aInt = 1;
+      assertEquals(aInt, 1);
+   }
+}
